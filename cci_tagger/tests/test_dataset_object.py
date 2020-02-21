1e5525dd9a863b350bed2b6c90eb2b23bd1efa1c
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-02-21 15:43:56 +0000

    edit testing

--- cci_tagger/tests/test_dataset_object.py
+++ cci_tagger/tests/test_dataset_object.py
@@ -10,17 +10,22 @@ __contact__ = 'richard.d.smith@stfc.ac.uk'
 
 from cci_tagger.dataset.dataset import Dataset
 from cci_tagger.facets import Facets
-from cci_tagger.dataset.dataset_tree import DatasetJSONMappings
+from cci_tagger_json import DatasetJSONMappings
 
 # filepath = '/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/ESACCI-SEASURFACESALINITY-L4-SSS-MERGED_OI_7DAY_RUNNINGMEAN_DAILY_25km-20120101-fv1.8.nc'
-filepath = '/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/ESACCI-SEAICE-L2P-SITHICK-SIRAL_CRYOSAT2-NH-20160101-fv2.0.nc'
+# filepath = '/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/ESACCI-SEAICE-L2P-SITHICK-SIRAL_CRYOSAT2-NH-20160101-fv2.0.nc'
+# filepath = '/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/CCI_GMB_GIS_DTU.nc'
+# filepath = '/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/landcover/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7.tif'
+filepath = '/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/sea_level/ESACCI-SEALEVEL-L4-MSLA-MERGED-19930115000000-fv02.nc'
 
 facets = Facets()
-json_mappings = DatasetJSONMappings('test_json_files')
+json_mappings = DatasetJSONMappings(['/Users/vdn73631/Documents/dev/cci-tagger/cci_tagger/tests/test_json_files/sea_level.json'])
 
 dataset = json_mappings.get_dataset(filepath)
 
 d = Dataset(dataset, json_mappings, facets)
+
+print(d.id)
 
 # print(d.process_dataset(max_file_count=1))
 
@@ -31,5 +36,5 @@ print(uris)
 labels = d.get_drs_labels(uris)
 print(labels)
 
-drs = d.generate_ds_id(labels)
+drs = d.generate_ds_id(labels,filepath)
 print(drs)
