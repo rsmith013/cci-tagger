3893171d44fb81d2bf02f1af13fa516ea7e53109
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-05-11 10:42:44 +0100

    Reduced testing dependency by referring to the script directly

--- cci_tagger/scripts/__init__.py
+++ cci_tagger/scripts/__init__.py
@@ -8,5 +8,4 @@ __copyright__ = 'Copyright 2018 United Kingdom Research and Innovation'
 __license__ = 'BSD - see LICENSE file in top-level package directory'
 __contact__ = 'richard.d.smith@stfc.ac.uk'
 
-from .check_json import TestJSONFile
 from .command_line_client import CCITaggerCommandLineClient
